ec5f4d2e9567bdcf92d3ced81c36a01e2f956c42
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-21 15:01:19 -0400

    Initialize inventory with product objects

--- order_management_system.js
+++ order_management_system.js
@@ -1,7 +1,7 @@
 let inventory = [
-    {name: "Iced Latte", price: 4.25, quantity: 35}
-    {name: "Expresso", price: 3.00, quantity: 50}
-    {name: "Guava Pastry", price: 3.25, quantity: 15}
+    {name: "Iced Latte", price: 4.25, quantity: 35},
+    {name: "Expresso", price: 3.00, quantity: 50},
+    {name: "Guava Pastry", price: 3.25, quantity: 15},
     {name: "Blueberry Muffin", price: 2.75, quantity: 20}
 ];
 console.log(inventory);
